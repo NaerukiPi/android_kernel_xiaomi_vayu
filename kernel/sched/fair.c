901802edfaeb00a2454b5c59c87a677a3e490ce7
NaerukiPi <naeruki0@gmail.com>
2026-06-06 19:58:19 +0700

    Revert "prefer_silver: add silver_core_mask bitmap and optimize find_best_silver_cpu"
    This reverts commit 0c7c45095e84b618cb61adf805c202170ec00653.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -88,7 +88,7 @@ static inline bool task_fits_max(struct task_struct *p, int cpu);
 static void walt_fixup_sched_stats_fair(struct rq *rq, struct task_struct *p,
 					u16 updated_demand_scaled,
 					u16 updated_pred_demand_scaled);
-static void __maybe_unused walt_fixup_nr_big_tasks(struct rq *rq, struct task_struct *p,
+static void walt_fixup_nr_big_tasks(struct rq *rq, struct task_struct *p,
 					int delta, bool inc);
 #endif /* CONFIG_SCHED_WALT */
 
@@ -8398,7 +8398,7 @@ static inline int wake_energy(struct task_struct *p, int prev_cpu,
 
 	if(!sched_feat(EAS_PREFER_IDLE)){
 
-static DEFINE_PER_CPU(cpumask_t, energy_cpus) __maybe_unused;
+static DEFINE_PER_CPU(cpumask_t, energy_cpus);
 
 /*
  * find_energy_efficient_cpu(): Find most energy-efficient target CPU for the
